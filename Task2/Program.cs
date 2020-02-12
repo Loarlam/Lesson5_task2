1dd48a13f05773a97dfb144ced3536940cf3ace2
Loarlam <messiah02@mail.ru>
2020-02-12 12:56:59 +0300

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Используя Visual Studio, создайте проект по шаблону Console Application.  
+Требуется:  
+Создать массив размерностью N элементов, заполнить его произвольными целыми значениями.  
+Вывести наибольшее значение массива, наименьшее значение массива, общую сумму элементов, среднее арифметическое всех элементов, вывести все нечетные значения. 
+*/
+using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array;
+            Random randomIntValue = new Random();
+
+            Console.Write("Количество элементов массива = ");
+            int arraySize = Int32.Parse(Console.ReadLine());
+            WorkWithAnArray findInArray = new WorkWithAnArray(arraySize);
+
+            array = new int[arraySize];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = randomIntValue.Next(1,100);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/WorkWithAnArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class WorkWithAnArray
+    {
+        int _numberOfArrayElements;
+        int[] _array;
+
+        public WorkWithAnArray(int numberOfArrayElements)
+        {
+            _numberOfArrayElements = numberOfArrayElements;
+            _array = new int[_numberOfArrayElements];
+        }
+
+        public int this[int index]
+        {
+            get { return _array[index]; }
+        }
+    }
+}
